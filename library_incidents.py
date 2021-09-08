77ad1f0e3d6b252e111901e1834936f9e6fe8770
morganmercredi <74431705+morganmercredi@users.noreply.github.com>
2021-09-08 00:55:39 -0500

    Added some visualizations for library incidents

--- library_incidents.py
+++ library_incidents.py
@@ -11,7 +11,7 @@ import seaborn as sns
 
 sns.set()
 
-# Read in the file
+# Read in the csv file
 incidents = pd.read_csv('Library_Incident_Reports.csv')
 
 # Clean up the table, set the date column as the index
@@ -19,29 +19,39 @@ incidents = incidents.drop(columns='ID')
 incidents = incidents.set_index('Date')
 incidents.index = pd.to_datetime(incidents.index)
 
+# Get the earliest recorded incident for each library
+print(incidents.groupby('Location').apply(lambda x: x.index.min()).sort_values())
+
 # Sort the number of incidents by library
 by_library = incidents.groupby('Location').size().sort_values(ascending=False)
 
-# Show the incidents for each library
+# Show the number of incidents for each library
 plt.figure()
 by_library.sort_values(ascending=True).plot(kind='barh')
 plt.gca().set_xlabel('Number of incidents')
 plt.gca().set_ylabel('Library')
-plt.gca().set_title('Reported Incidents by Library')
+plt.gca().set_title('Reported Library Incidents (2012 - 2021)')
 
 # Group incidents by seriousness
 print(incidents.groupby('Serious').size())
 
 # Show only serious incidents
-# For now, the x-axis shows fractional ticks (2.5, 7.5, etc)
 plt.figure()
 incidents[incidents.Serious == 'Yes'].groupby('Type').size().sort_values().plot(kind='barh')
 plt.gca().set_xlabel('Number of incidents')
 plt.gca().set_ylabel('Incident type')
-plt.gca().set_title('Distribution of Serious Incidents')
+plt.gca().set_title('Serious Library Incidents (2012 - 2021)')
+plt.gca().set_xticks(range(0, 25, 5))
 
 # Sort incidents by type
-by_type = incidents.groupby('Type').size().sort_values(ascending=False)
+by_type = incidents.groupby('Type').size().sort_values()
+
+# Show total incidents by type
+plt.figure()
+by_type.plot(kind='barh')
+plt.gca().set_xlabel('Number of incidents')
+plt.gca().set_ylabel('Incident type')
+plt.gca().set_title('Reported Library Incidents (2012 - 2021)')
 
 # Get the number of incidents by year
 by_year = incidents.groupby(incidents.index.year).size()
@@ -51,7 +61,7 @@ plt.figure()
 by_year.plot(kind='bar')
 plt.gca().set_xlabel('Year')
 plt.gca().set_ylabel('Number of incidents')
-plt.gca().set_title('Yearly Library Incidents')
+plt.gca().set_title('Total Yearly Library Incidents')
 
 # Show the incidents for the year 2012
 print(incidents.loc['2012']) # there are only two incidents recorded
@@ -64,7 +74,7 @@ plt.figure()
 by_month.plot(kind='bar')
 plt.gca().set_xlabel('Month')
 plt.gca().set_ylabel('Number of incidents')
-plt.gca().set_title('Library Incidents by Month')
+plt.gca().set_title('Total Library Incidents by Month')
 plt.gca().set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 
                            'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
 
@@ -76,26 +86,64 @@ plt.figure()
 by_day_of_week.plot(kind='bar')
 plt.gca().set_xlabel('Day of week')
 plt.gca().set_ylabel('Number of incidents')
-plt.gca().set_title('Library Incidents by Day of Week')
+plt.gca().set_title('Total Library Incidents by Day of Week')
 plt.gca().set_xticklabels(['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'])
-
-# Get the daily number of daily incidents
-daily_incidents = incidents.resample('D', kind='period').size()
 
 # Get the number of incidents by year and type
 by_year_and_type = incidents.pivot_table(index=incidents.index.year,
                                          columns='Type',
                                          aggfunc='size').fillna(0)
 
+# Pick a few types of incidents and show how they've varied over the years
+plt.figure()
+by_year_and_type[['Inappropriate Behaviour', 'Intoxication', 'Assault']].plot(kind='bar').legend(loc='best')
+plt.gca().set_xlabel('Year')
+plt.gca().set_ylabel('Number of incidents')
+plt.gca().set_title('Reported Library Incidents Over Time')
+
 # Get the number of incidents by year and library
 by_year_and_library = incidents.pivot_table(index=incidents.index.year,
                                             columns='Location',
                                             aggfunc='size').fillna(0)
 
+# Pick a few libraries and show how incidents varied over the years
+plt.figure()
+by_year_and_library[['St. Vital', 'Louis Riel', 'Cornish']].plot(kind='bar').legend(loc='upper left')
+plt.gca().set_xlabel('Year')
+plt.gca().set_ylabel('Number of incidents')
+plt.gca().set_title('Reported Library Incidents Over Time')
+
 # Get the number of incidents by year, library, and type
 by_year_library_type = incidents.pivot_table(index=incidents.index.year,
                                              columns=['Location', 'Type'],
                                              aggfunc='size').fillna(0)
+
+# Get the daily number of daily incidents
+daily_incidents = incidents.resample('D', kind='period').size()
+
+# Resample to weekly incidents and show them for 2018 and 2019
+fig, ax = plt.subplots(2, figsize=(10, 10))
+daily_incidents.resample('W', kind='period').sum()['2018'].plot(ax=ax[0])
+daily_incidents.resample('W', kind='period').sum()['2019'].plot(ax=ax[1]) 
+ax[0].set_ylabel('Number of incidents')
+ax[1].set_ylabel('Number of incidents')
+ax[0].set_ylim([0, 30]) 
+ax[1].set_ylim([0, 30])
+ax[0].set_title('Weekly Incidents in 2018 and 2019')
+
+
+# Get the daily number of daily incidents at the Millennium library
+daily_millennium_incidents = incidents[incidents['Location'] == 'Millennium'].resample('D', kind='period').size()
+
+# Resample to weekly incidents and show them for 2018 and 2019
+fig, ax = plt.subplots(2, figsize=(10, 10))
+daily_millennium_incidents.resample('W', kind='period').sum()['2018'].plot(ax=ax[0])
+daily_millennium_incidents.resample('W', kind='period').sum()['2019'].plot(ax=ax[1]) 
+ax[0].set_ylabel('Number of incidents')
+ax[1].set_ylabel('Number of incidents')
+ax[0].set_ylim([0, 30]) 
+ax[1].set_ylim([0, 30])
+ax[0].set_title('Weekly Incidents at Millennium Library in 2018 and 2019')
 
 plt.show()
 
