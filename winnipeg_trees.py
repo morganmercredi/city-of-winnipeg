9847406bb90db741dd6f12829f98558241f1e6a8
morgan <morgan.mercredi@gmail.com>
2021-12-21 01:50:15 -0600

    .

--- winnipeg_trees.py
+++ winnipeg_trees.py
@@ -45,20 +45,20 @@ nbhd['the_geom'] = nbhd['the_geom'].apply(wkt_loads)
 wards['the_geom'] = wards['the_geom'].apply(wkt_loads)
 
 # Convert neighbourhood and ward data to GeoDataFrames
-nbhd_gdf = gpd.GeoDataFrame(nbhd.copy(), geometry='the_geom')
+nbhd = gpd.GeoDataFrame(nbhd.copy(), geometry='the_geom')
 wards = gpd.GeoDataFrame(wards.copy(), geometry='the_geom')
 
 # Set the crs to lat/lon
-nbhd_gdf = nbhd_gdf.set_crs("EPSG:4326")
+nbhd = nbhd.set_crs("EPSG:4326")
 wards = wards.set_crs("EPSG:4326")
 
 # Convert to a projected crs for Manitoba (approximately)
-nbhd_gdf = nbhd_gdf.to_crs('EPSG:32614')
+nbhd = nbhd.to_crs('EPSG:32614')
 wards = wards.to_crs('EPSG:32614')
 
 # Get the area of the neighbourhoods and wards (in square kilometres)
 wards['Area'] = wards.area/1e6
-nbhd_gdf['Area'] = nbhd_gdf.area/1e6
+nbhd['Area'] = nbhd.area/1e6
 
 # Remove the 'x', 'y', and 'ded_tag_no' columns
 trees.drop(columns=['x', 'y', 'ded_tag_no'], inplace=True)
@@ -86,28 +86,31 @@ trees_by_neighbourhood = trees.groupby('nbhd').size().sort_values(ascending=Fals
 # Let's check if the city neighbourhood list matches the neighbourhood list
 # from the tree inventory data
 # The neighbourhood in the tree inventory are all caps. Change to lower case.
-nbhd_gdf.Name = nbhd_gdf.Name.str.lower()
+nbhd.Name = nbhd.Name.str.lower()
 trees_by_neighbourhood.index = trees_by_neighbourhood.index.str.lower()
 
 # Get the neighbourhoods in the tree inventory that aren't in the city neighbourhood list
-print(trees_by_neighbourhood[~trees_by_neighbourhood.index.isin(nbhd_gdf.Name)].index.to_list())
+print(trees_by_neighbourhood[~trees_by_neighbourhood.index.isin(nbhd.Name)].index.to_list())
 
 # Get the neighbourhoods in the city neighbourhood list that aren't in the tree inventory data
-print(nbhd_gdf[~nbhd_gdf.Name.isin(trees_by_neighbourhood.index)].Name.to_list())
+print(nbhd[~nbhd.Name.isin(trees_by_neighbourhood.index)].Name.to_list())
 
 # So, five neighbourhoods aren't in the tree inventory dataset
 # Merge the trees_by_neighbourhood data to the city neighbourhood dataset
-nbhd_gdf = nbhd_gdf.merge(trees_by_neighbourhood.rename('Number of trees'),
-                            left_on='Name', right_index=True)
+nbhd = nbhd.merge(trees_by_neighbourhood.rename('Number of trees'),
+                  left_on='Name', right_index=True)
 
 # Add a column for the density of trees per neighbourhood
-nbhd_gdf['Density'] = nbhd_gdf['Number of trees'].div(nbhd_gdf['Area'])
+nbhd['Density'] = nbhd['Number of trees'].div(nbhd['Area'])
 
 # Plot the tree density over the neighbourhood map
 plt.figure();
-nbhd_gdf.plot('Density', legend=True, cmap='Greens');
+nbhd.plot('Density', legend=True, cmap='Greens');
 plt.gca().set_title('Tree Density by Neighbourhood (km$^{-2}$)');
 plt.axis('off');
+
+# Get the top 10 most tree dense neighbourhoods
+print(nbhd.sort_values(by='Density', ascending=False)[['Name', 'Density']][:10])
 
 def get_most_treed_nbhds(group, n=5):
     """Get the n-most treed neighbourhoods in a group."""
@@ -141,9 +144,9 @@ tree_species_stats = trees.groupby('common')['dbh'].agg(['mean', 'std'])
 # in measured diameter
 plt.figure()
 tree_species_stats.plot.scatter(x='mean', y='std', c='b')
-plt.gca().set_xlabel('Mean measured diameter')
+plt.gca().set_xlabel('Average measured diameter')
 plt.gca().set_ylabel('Standard deviation in measured diameter')
-plt.gca().set_title('Mean and Standard Deviation in Measured Diameter by Tree Species')
+plt.gca().set_title('Average and Standard Deviation in Measured Diameter by Tree Species')
 
 # Show the distribution of diameters for American Elm (e.g.)
 plt.figure()
@@ -154,19 +157,27 @@ plt.gca().set_title('Distribution of American Elm Diameters')
 
 # Here, I try to extract latitudes and longitues from the point column
 # This was before I found the Shapely and Geopandas packages
-def get_lat_lon(geom):
-    """Extract latitude and longitude from geometry column"""
-    geom = geom.split()
-    lat = float(geom[2].rstrip(')'))
-    lon = float(geom[1].lstrip('('))
-    return lat, lon
-
+#def get_lat_lon(geom):
+#    """Extract latitude and longitude from geometry column"""
+#    geom = geom.split()
+#    lat = float(geom[2].rstrip(')'))
+#    lon = float(geom[1].lstrip('('))
+#    return lat, lon
+#
 # Extract the latitudes and longitudes from the geometry column
 # This is a series of tuples
-lat_lon = trees['the_geom'].apply(get_lat_lon)
-
+#lat_lon = trees['the_geom'].apply(get_lat_lon)
+#
 # Split the tuples into separate columns in the tree table
-trees[['lat', 'lon']] = lat_lon.to_list()
+#trees[['lat', 'lon']] = lat_lon.to_list()
+
+# Convert the tree dataset into a GeoDataFrame
+trees = gpd.GeoDataFrame(trees.copy(), geometry='the_geom')
+trees = trees.set_crs("EPSG:4326")
+
+# Create separate columns for latitude and longitude
+trees['Longitude'] = trees.the_geom.x
+trees['Latitude'] = trees.the_geom.y
 
 # Use kernel density estimation on the tree locations
 # Do a grid search over a few different bandwidths (this takes an hour on my PC)
