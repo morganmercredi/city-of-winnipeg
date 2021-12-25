f739f307f983588be192ad11f2cd6868282019ad
morgan <morgan.mercredi@gmail.com>
2021-12-25 15:32:14 -0600

    .

--- winnipeg_trees.py
+++ winnipeg_trees.py
@@ -166,15 +166,31 @@ trees['Latitude'] = trees.the_geom.y
 bandwidths = [0.0001, 0.0005, 0.001]
 grid = GridSearchCV(KernelDensity(kernel='gaussian'), {'bandwidth': bandwidths},
                     cv=3, n_jobs=-1)
-grid.fit(np.array(trees[['lon', 'lat']]));
+grid.fit(np.array(trees[['Longitude', 'Latitude']]))
 print(grid.best_params_) # 0.0005 is the best choice
 
 # Choose the best model
 model = grid.best_estimator_
 
+# Overlay the tree inventory with a city of Winnipeg boundary map
+# Path to Winnipeg boundary file
+city_map = 'https://data.winnipeg.ca/api/views/2nyq-f444/rows.csv?accessType=DOWNLOAD'
+
+# Load the file and convert to a GeoDataFrame
+wpg_borders = pd.read_csv(city_map)
+wpg_borders['the_geom'] = wpg_borders['the_geom'].apply(wkt_loads)
+wpg_borders = gpd.GeoDataFrame(wpg_borders.copy(), geometry='the_geom')
+wpg_borders = wpg_borders.set_crs('EPSG:4326')
+ax = wpg_borders.boundary.plot()
+ax.axis('off')
+
+# Use the limits for the city map
+xmin, xmax = ax.get_xlim()
+ymin, ymax = ax.get_ylim()
+
 # Create a set of points to predict the density
-x = np.linspace(-96.95, -97.35, 200)
-y = np.linspace(49.7, 49.98, 200)
+x = np.linspace(xmin, xmax, 200)
+y = np.linspace(ymin, ymax, 200)
 xx, yy = np.meshgrid(x, y)
 
 # Get a set of density predictions
@@ -183,6 +199,6 @@ pred = np.exp(log_pred)
 
 # Show the tree distribution
 levels = np.linspace(pred.min(), pred.max(), 100)
-plt.contourf(xx, yy, pred, alpha=0.3, levels=levels)
+ax.contourf(xx, yy, pred, alpha=0.3, levels=levels)
 
 plt.show()
